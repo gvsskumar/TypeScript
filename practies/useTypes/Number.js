f1982f9e6261e8750f2a334f6c95ea7d9042c968
gvsskumar <kumar.developer0701@gmail.com>
2023-07-04 14:42:05 +0530

    Installation Type Script

---
+++ practies/useTypes/Number.js
@@ -0,0 +1,6 @@
+var a = 2;
+var b = 6;
+var multiply = function (a, b) {
+    return a * b;
+};
+console.log(multiply(+a, +b));
---
+++ practies/useTypes/Number.ts
@@ -0,0 +1,6 @@
+const a =2;
+const b =6;
+const multiply =(a:number,b:number)=>{
+    return a*b;
+}
+console.log(multiply(+a,+b));
