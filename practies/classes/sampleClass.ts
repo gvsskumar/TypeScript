d48bf9dba816a5c6c6c0951986edc47940edd4ba
gvsskumar <kumar.developer0701@gmail.com>
2023-07-08 14:36:47 +0530

    this keyword using copy function

--- practies/classes/sampleClass.ts
+++ practies/classes/sampleClass.ts
@@ -3,9 +3,11 @@ class Department {
     constructor(n:string){
         this.name =n;
     }
-    describe() {
+    describe(this:Department) {
         console.log('Department '+this.name);
     }
 }
 const obj = new Department('Web Development');
 obj.describe();
+const objCopy ={name:"SEO",describe:obj.describe};
+objCopy.describe();
